557f22acf7666e65fcb3f9454561d2d128b38126
goproslowyo <goproslowyo@users.noreply.github.com>
2022-03-06 03:02:57 -0800

    Added config hot-reloading & custom vanity names
    * Config hot-reloading: You should be able to edit `config.json` and the
      bot should detect the changes when a new "event" is processed.
    
    * Using `custom_names` in the configuration file you can specify custom
      names for each user to change how trumpet announces their arrival!
    
    * Updated the example `config.json.example` configuration file.
    
    h/t @Colenk

--- config.go
+++ config.go
@@ -1,40 +1,55 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
 )
 
 type Config struct {
-	FfmpegPath                      string   `json:"ffmpeg_path"`
-	GoogleServiceAccountCredentials string   `json:"google_service_account_credentials"`
-	IgnoreList                      []string `json:"ignore_list"`
-	Prefix                          string   `json:"prefix"`
-	Token                           string   `json:"token"`
-	UserAudioPath                   string   `json:"user_audio_path"`
-	YtdlPath                        string   `json:"youtube-dl_path"`
+	CustomNames                     map[string]string `json:"custom_names"`
+	FfmpegPath                      string            `json:"ffmpeg_path"`
+	GoogleServiceAccountCredentials string            `json:"google_service_account_credentials"`
+	IgnoreList                      []string          `json:"ignore_list"`
+	Prefix                          string            `json:"prefix"`
+	Token                           string            `json:"token"`
+	UserAudioPath                   string            `json:"user_audio_path"`
+	YtdlPath                        string            `json:"youtube-dl_path"`
+	ConfigHash                      string
 }
 
 const configFile = "config.json"
 
 const tokenDefaultString = "insert your discord bot token here"
 
+func (c *Config) hashConfig(config []byte) string {
+	hash := fmt.Sprintf("%x", sha256.Sum256(config))
+	return hash
+}
+
 func ReadConfig(cfg *Config) error {
 	configData, err := os.ReadFile(configFile)
 	if err != nil {
 		return errors.New("unable to read config file: " + err.Error())
 	}
+	newHash := cfg.hashConfig(configData)
 
-	json.Unmarshal(configData, cfg)
-	if err != nil {
-		return errors.New("unable to decode config file: " + err.Error())
+	if cfg.ConfigHash != newHash || cfg.ConfigHash == "" {
+		json.Unmarshal(configData, cfg)
+		if err != nil {
+			return errors.New("unable to decode config file: " + err.Error())
+		}
+		cfg.ConfigHash = newHash
+		logger.Sugar().Infof("Config file (re)loaded, hash: %s\n", cfg.ConfigHash)
 	}
 	return nil
 }
 
 func WriteDefaultConfig() error {
 	data, err := json.MarshalIndent(Config{
+		CustomNames:                     map[string]string{},
 		FfmpegPath:                      "ffmpeg",
 		GoogleServiceAccountCredentials: "google-translate-api-credentials.json",
 		IgnoreList:                      []string{},
